65438982f195759a088f3ac2ded7d4723ab2e8f1
DreaMeR <DreaMeR@DREAMER>
2024-07-12 11:11:40 +0600

    Добавьте файлы проекта.

---
+++ ExitControl/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExitControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitWord; 
+            bool isNotExit = true;
+
+            while (isNotExit)
+            {
+                Console.WriteLine("Hello World");
+                Console.WriteLine("Для того чтобы выйти из приложения введите слово <exit>");
+
+                exitWord = Console.ReadLine();
+
+                if (exitWord == "exit")
+                {
+                    isNotExit = false;
+                }
+            }
+        }
+    }
+}
